bde2a8ece53b59205a3669198eb8676d57bf29f5
David Torrez <dtorrez1996@gmail.com>
2025-07-24 17:16:37 -0400

    Add translations in English

---
+++ src/i18n/en.ts
@@ -0,0 +1,49 @@
+export default {
+  languageLabel: "Language",
+  languageOptions: {
+    es: "Spanish",
+    en: "English",
+  },
+  nav: {
+    home: "Home",
+    projects: "Projects",
+    about: "About",
+    contact: "Contact",
+    themeToggle: {
+      light: "Light",
+      dark: "Dark",
+      system: "System",
+    },
+  },
+  hero: {
+    availableBadge: "Available to work",
+    title: "Hey, I'm David Torrez",
+    description:
+      "Web Developer based in Vancouver, Canada. I focus on creating Responsive and User-friendly interfaces.",
+    resumeButton: "Resume",
+  },
+  projects: {
+    title: "Projects",
+    project1Description:
+      "Real Outfit allows users to browse products, view detailed product information, and manage their shopping cart. With a clean, responsive interface and modern tech stack, this platform is built for a smooth and enjoyable shopping experience.",
+    project2Description:
+      "Welcome to Up Immigration! This website is designed to clearly present various Canadian immigration pathways, such as study permits, work permits, and more. With a clean and accessible interface, users can easily explore their options and understand the steps involved in each immigration process.",
+    project3Description:
+      "Finance M8 is designed to help you effortlessly track your incomes and expenses. With a simple and intuitive interface, you can easily add new financial records, update existing ones, and delete records you no longer need.",
+    codeButton: "Code",
+  },
+  about: {
+    title: "About",
+    paragraph1:
+      "My name is David Torrez, I realized I wanted to be in this industry around 2021 when I was surfing the internet and looking at some websites with amazing effects and animations, since then I started watching tutorials about CSS and HTML, as well as taking some online courses.",
+    paragraph2:
+      "I took a Bachelor's degree in Bilingual Education in Colombia and graduated in 2021, after a couple of years trying to learn about Web Development on my own, I wanted to level up, therefore, I decided to come to Canada to pursue a formal education in Web Development at CICCC.",
+    paragraph3:
+      "When I'm not coding, I'm usually watching a movie, riding my bicycle, working out, or spending time with relatives or friends.",
+  },
+  footer: {
+    createdWith: "Created with Astro",
+    about: "About",
+    contact: "Contact",
+  },
+};
