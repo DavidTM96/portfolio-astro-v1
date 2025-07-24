99ef5de99209095b8a6782f0491129a5183168c4
David Torrez <dtorrez1996@gmail.com>
2025-07-24 15:48:14 -0400

    Configure i18n routing, add default language and other language options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
 import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: "https://portfolio-davidtm96.netlify.app/",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
