9d92963bb77e3739247b720d224d6cea0b7f9761
David Torrez <dtorrez1996@gmail.com>
2025-07-24 17:16:46 -0400

    Add translations in Spanish

---
+++ src/i18n/es.ts
@@ -0,0 +1,49 @@
+export default {
+  languageLabel: "Idioma",
+  languageOptions: {
+    es: "Español",
+    en: "Inglés",
+  },
+  nav: {
+    home: "Inicio",
+    projects: "Proyectos",
+    about: "Sobre mí",
+    contact: "Contacto",
+    themeToggle: {
+      light: "Claro",
+      dark: "Oscuro",
+      system: "Sistema",
+    },
+  },
+  hero: {
+    availableBadge: "Disponible para trabajar",
+    title: "¡Hola, soy David Torrez!",
+    description:
+      "Desarrollador web con sede en Vancouver, Canadá. Me especializo en crear interfaces responsivas y fáciles de usar.",
+    resumeButton: "Currículum",
+  },
+  projects: {
+    title: "Proyectos",
+    project1Description:
+      "Real Outfit permite a los usuarios explorar productos, ver información detallada y administrar su carrito de compras. Con una interfaz clara y responsiva y tecnología moderna, esta plataforma está diseñada para una experiencia de compra fluida y agradable.",
+    project2Description:
+      "¡Bienvenido a Up Immigration! Este sitio web está diseñado para presentar claramente las diversas vías de inmigración a Canadá, como permisos de estudio, permisos de trabajo y más. Con una interfaz clara y accesible, los usuarios pueden explorar fácilmente sus opciones y comprender los pasos de cada proceso de inmigración.",
+    project3Description:
+      "Finance M8 está diseñado para ayudarte a controlar fácilmente tus ingresos y gastos. Con una interfaz sencilla e intuitiva, puedes añadir fácilmente nuevos registros financieros, actualizar los existentes y eliminar los que ya no necesites.",
+    codeButton: "Código",
+  },
+  about: {
+    title: "Sobre mí",
+    paragraph1:
+      "Mi nombre es David Torrez, me di cuenta que quería estar en esta industria alrededor de 2021 cuando navegaba en internet y veía algunos sitios web con efectos y animaciones increíbles, desde entonces comencé a ver tutoriales sobre CSS y HTML, además de tomar algunos cursos en línea.",
+    paragraph2:
+      "Estudié Licenciatura en Educación Bilingüe en Colombia y me gradué en el 2021, después de un par de años intentando aprender sobre Desarrollo Web por mi cuenta, quería subir de nivel, por lo tanto, decidí venir a Canadá a realizar una educación formal en Desarrollo Web en CICCC.",
+    paragraph3:
+      "Cuando no estoy programando, normalmente estoy viendo una película, montando en bicicleta, haciendo ejercicio o pasando tiempo con familiares o amigos.",
+  },
+  footer: {
+    createdWith: "Creado con Astro",
+    about: "Sobre mí",
+    contact: "Contacto",
+  },
+};
